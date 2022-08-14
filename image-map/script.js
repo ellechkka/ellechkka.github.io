091a7b4e4aefe15911f16c5555ecc828aa4a6337
Романова Элина <72344565+ellechkka@users.noreply.github.com>
2022-08-14 16:27:39 +0300

    Add files via upload

---
+++ image-map/script.js
@@ -0,0 +1,9 @@
+const function1 = () => alert("this is a charger");
+const function2 = () => alert("this is airpods");
+const function3 = () => alert("this is a film camera");
+const function4 = () => alert("this is a book");
+const function5 = () => alert("this is apple pencil");
+const function6 = () => alert("this is an ipad");
+const function7 = () => alert("this is a hand cream");
+const function8 = () => alert("this is a lip balm");
+const function9 = () => alert("this is a scrunchie");
